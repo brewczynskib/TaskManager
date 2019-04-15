b96abad66131273cbbced139ed72ed4cda8d9871
brewczynskib <bartekbrewcz@gmail.com>
2019-04-15 10:30:52 +0200

    client attributes read-only

--- client/client.py
+++ client/client.py
@@ -4,22 +4,25 @@ import getopt
 class Client:
 
 	def __init__(self, login, password):
-		self._login = login
-		self._password = password
-		self._online = False
+		self.__login = login
+		self.__password = password
+		self.__online = False
 
 	def __repr__(self):
 		class_name = type(self).__name__
 		return '{}{!r}'.format(class_name, self._login)
 
+	@property
 	def get_login(self):
-		return self._login
+		return self.__login
 
+	@property
 	def get_password(self):
-		return self._password
+		return self.__password
 
 	def set_online(self):
-		self._online = True
+		self.__online = True
 
+	@property
 	def get_status(self):
-		return self._online
+		return self.__online
